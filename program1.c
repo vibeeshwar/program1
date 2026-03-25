0c7aeac2c98fb7b33aaa35cb49cebb79a8a6e4e6
vibeeshwar <vibee.24tn@gmail.com>
2026-03-25 14:15:15 +0530

    Update program1.c

--- program1.c
+++ program1.c
@@ -1,1 +1,38 @@
-i am vibee
+#include <stdio.h>
+int main()
+{
+int roll, choice;
+char name[30];
+char attendance;
+while(1)
+{
+printf("\nSTUDENT ATTENDANCE MANAGEMENT SYSTEM\n");
+printf("1. Add Student Attendance\n");
+printf("2. Exit\n");
+printf("Enter your choice: ");
+scanf("%d",&choice);
+if(choice==1)
+{
+printf("Enter Roll Number: ");
+scanf("%d",&roll);
+printf("Enter Student Name: ");
+scanf("%s",name);
+printf("Enter Attendance (P for Present / A for Absent): ");
+scanf(" %c",&attendance);
+printf("\nAttendance Recorded Successfully!\n");
+printf("Roll No: %d\n",roll);
+printf("Name: %s\n",name);
+printf("Attendance: %c\n",attendance); 
+}
+else if(choice==2)
+{
+printf("Program Ended\n");
+break;
+}
+else
+{
+printf("Invalid Choice\n");
+}
+}
+return 0;
+}
